acc38e40a1ad387250924b0c02b34e1ee1b02d11
Nisha0202 <nishajabatunnessa@gmail.com>
2024-10-04 23:55:21 +0600

    matvhing path

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,9 +6,19 @@ export {default} from 'next-auth/middleware'
 export async function middleware(request:NextRequest) {
 
     const token = getToken({req: request});
+    const url = request.nextUrl;
+
 
     return NextResponse.redirect(new URL('home', request.url))
 
 
     
+}
+
+// Matching paths
+export const config ={
+    matcher: ['/sigup',
+        '/signin',
+        '/verify/:path*'
+        ]
 }
