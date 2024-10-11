f612890da7288561cb1377d0f55024ec329e707f
Nisha0202 <nishajabatunnessa@gmail.com>
2024-10-12 03:44:17 +0600

    email style improved

--- src/email/VerificationEmail.tsx
+++ src/email/VerificationEmail.tsx
@@ -7,7 +7,7 @@ import {
     Section,
     Text,
     Button,
-    Container, 
+    Container,
     Body
 } from '@react-email/components'
 
@@ -18,17 +18,6 @@ interface VerificationEmailProps {
 
 export default function VerificationEmail({ username, verifycode }: VerificationEmailProps) {
     return (
-        // <Html>
-        //     <Head>
-        //         <title>Verify Email</title>
-        //     </Head>
-        //     <Preview>Your verification email</Preview>
-        //     <Section>
-        //         <Heading>Hi {username},</Heading>
-        //         <Text>Your OTP for verification is: {verifycode}</Text>
-        //         <Button href="#">Verify Now</Button>
-        //     </Section>
-        // </Html>
 
         <Html>
             <Head>
@@ -38,10 +27,12 @@ export default function VerificationEmail({ username, verifycode }: Verification
             <Body style={{ backgroundColor: '#f5f5f5', padding: '20px' }}>
                 <Container style={{ backgroundColor: '#ffffff', borderRadius: '8px', padding: '20px' }}>
                     <Section>
-                        <Heading style={{ fontSize: '24px', color: '#333' }}>Hello, {username}</Heading>
+                        <Heading style={{ fontSize: '20px', color: '#333' }}>Hi, {username}</Heading>
                         <Text style={{ fontSize: '16px', color: '#555' }}>
+                            Thank you for starting the journey with AnonFeedback! <br />
                             Your OTP for verification is: <strong>{verifycode}</strong>
                         </Text>
+
                         <Button href="#" style={{ backgroundColor: '#007bff', color: '#fff', padding: '10px 20px', borderRadius: '5px' }}>
                             Verify Now
                         </Button>
