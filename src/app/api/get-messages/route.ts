0336139d17bd50e0fd4f0ea687f04694d990911f
Nisha0202 <nishajabatunnessa@gmail.com>
2024-10-13 22:15:03 +0600

    fixed

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -1,6 +1,6 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
-import { getServerSession, User } from "next-auth";
+import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/options";
 import mongoose from "mongoose";
 
