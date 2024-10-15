0bfee9ce0db182f1081c42d5722c127a2138987a
Nisha0202 <nishajabatunnessa@gmail.com>
2024-10-15 20:14:00 +0600

    meta data

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    domains: ['https://i.pinimg.com'],  // Replace with the domain of the external image
+    domains: ['i.pinimg.com'],  // Replace with the domain of the external image
   },};
 
 export default nextConfig;
