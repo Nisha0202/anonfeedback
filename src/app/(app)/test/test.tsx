f739473bc773b3af7580acfa29d041d8b25ff558
Nisha0202 <nishajabatunnessa@gmail.com>
2024-10-27 00:18:13 +0600

    memo cards

---
+++ src/app/(app)/test/test.tsx
@@ -0,0 +1,11 @@
+ // const fetchAcceptMessage = useCallback(async () => {
+  //   setSwitchLoading(true);
+  //   try {
+  //     const response = await axios.get('/api/accept-messages');
+  //     setValue('acceptMessages', response.data.isAcceptingMessage);
+  //   } catch (error) {
+  //     handleAxiosError(error as AxiosError<ApiResponse>, toast);
+  //   } finally {
+  //     setSwitchLoading(false);
+  //   }
+  // }, [setValue]);
